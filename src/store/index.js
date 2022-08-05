42990e9cbe02c1f85ab199fe2db9f454367a385e
Ryan Cabanas <ryangithub777@gmail.com>
2022-08-04 20:35:14 -0400

    End of Lesson 243: Assign a single "Slice" to the `reducer` property of the `configureStore` function config object.

--- src/store/index.js
+++ src/store/index.js
@@ -25,7 +25,7 @@ const counterSlice = createSlice({
 });
 
 const store = configureStore({
-  reducer: { counter: counterSlice.reducer },
+  reducer: counterSlice.reducer,
 });
 
 export default store;
